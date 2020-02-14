604f4d3fa39d3240f9ff88c87f8d7669830afcb0
Stephen Blair <stephenjohnblair@outlook.com>
2020-02-14 06:27:46 +0000

    Added ForecastDetails component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,17 @@ import PropTypes from 'prop-types';
 import Title from './title';
 import LocationDetails from './location-details';
 import ForecastSummaries from './forecast-summaries';
+import ForecastDetails from './forecast-details';
 import '../styles/app.css';
 
 const App = props => (
   <div className="title-header">
     <Title />
     <div className="forecast">
-        <LocationDetails city={props.location.city} country={props.location.country} />
-        <ForecastSummaries forecasts={props.forecasts} />
-      </div>
+      <LocationDetails city={props.location.city} country={props.location.country} />
+      <ForecastSummaries forecasts={props.forecasts} />
+      <ForecastDetails forecast={props.forecasts[0]} />
+    </div>
   </div>
 );
 
--- src/components/forecast-details.jsx
+++ src/components/forecast-details.jsx
@@ -1,22 +1,31 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ForecastDetails = ({ date }, { temperature }, { humidity }, { wind }) => (
-  <div classsName="forecast-detail">
-    <div className="forecast-date">
-      <span>{date}</span>
-    </div>
-    <div className="temperature">
+const ForecastDetails = ({ forecast }) => (
+  <div classsName="forecast-details" id="thisbit">
+    <div className="forecast-details-date">
       <span>
-        {temperature}
-        °C
+        {forecast.date}
+        {console.log(forecast)}
       </span>
-    </div>
-    <div className="humidity">
-      <span>{humidity}</span>
-    </div>
-    <div className="wind">
-      <span>{wind}</span>
+      <div className="forecast-details-temperature">
+        <span>
+          Temperataure <br />
+          Max:
+          {forecast.temperature.max} <br />
+          Min:
+          {forecast.temperature.min}
+        </span>
+      </div>
+      <div className="forecast-details-humidity">Humidity: {forecast.humidity}</div>
+      <div className="forecast-details-wind">
+        Wind <br />
+        Speed: {forecast.wind.speed} <br />
+        Direction:
+        {forecast.wind.direction}
+      </div>
     </div>
   </div>
 );
@@ -24,7 +33,5 @@ const ForecastDetails = ({ date }, { temperature }, { humidity }, { wind }) => (
 ForecastDetails.propTypes = {
   date: PropTypes.number.isRequired,
   temperature: PropTypes.number.isRequired,
-  humidity: PropTypes.number.isRequired,
-  wind: PropTypes.string.isRequired,
 };
 export default ForecastDetails;
